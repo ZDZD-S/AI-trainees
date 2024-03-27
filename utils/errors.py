1fb335a3e32b52560a8a110a50379b3bf65c79ad
Zyad <zzee7855@gmail.com>
2024-03-27 11:37:08 +0300

    mini ATS program

---
+++ utils/errors.py
@@ -0,0 +1,19 @@
+# A generic error message for unexpected situations that don't fit the specific categories above.
+GENERAL_ERROR = "An unexpected error has occurred."
+# Used when the user provides input that doesn't meet the expected format or criteria.
+INVALID_INPUT_ERROR = "Invalid input provided."
+# Utilized when a requested piece of data or resource cannot be located.
+DATA_NOT_FOUND_ERROR = "Requested data not found."
+# Indicates a failure in establishing a connection to the database.
+DATABASE_CONNECTION_ERROR = "Failed to connect to the database."
+# Represents an error encountered while attempting to write to a file.
+FILE_WRITE_ERROR = "Error occurred while writing to the file."
+# Used in scenarios where an access or action is attempted without proper authorization.
+UNAUTHORIZED_ACCESS_ERROR = "Unauthorized access attempt detected."
+
+# The input errors to the files
+INVALID_EMAIL_ERROR = "Invalid email format. Please enter a valid email."
+INVALID_PHONE_NUMBER_ERROR = "Invalid input. Please enter a number up to {length} digits."
+INVALID_LINKEDIN_URL_ERROR = "Invalid LinkedIn URL. Please enter a valid URL starting with https://www.linkedin.com/"
+EMPTY_INPUT_ERROR = "Input cannot be empty."
+INVALID_SALARY_RANGE_ERROR = "Invalid salary range format."
